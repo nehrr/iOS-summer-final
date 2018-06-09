e041d2062652edd446ee79804eda50818e184ad4
nehrr <iamkapitaan@gmail.com>
2018-06-09 09:55:39 +0200

    implemented tableView protocol in details

--- iOS-summer-final/Controllers/DetailsViewController.swift
+++ iOS-summer-final/Controllers/DetailsViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class DetailsViewController: UIViewController {
-    
+class DetailsViewController: UIViewController, UITableViewDataSource {
+ 
     var aCity: City?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getData()
+//        self.getData()
         
         if let cityName = aCity?.name {
             navigationItem.title = cityName
@@ -35,24 +35,32 @@ class DetailsViewController: UIViewController {
         if let cityLat = aCity?.coordinates.latitude, let cityLong = aCity?.coordinates.longitude {
             var url: String = "https://api.darksky.net/forecast/0d387f3d301b383589fe5b5350cf9a77/\(cityLat),\(cityLong)?units=si"
             print(url)
-                    Alamofire.request("google.fr").responseJSON { response in
-            
-                        switch response.result {
-            
-                        case .success:
-                            if let value = response.result.value {
-                                let json = JSON(value)
-                                for recipe in json["drinks"].arrayValue {
-//                                    self.cocktail?.update(recipe)
-                                }
-                            }
-//                            self.tableView.reloadData()
-            
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
+//            Alamofire.request("google.fr").responseJSON { response in
+//                
+//                switch response.result {
+//                    
+//                case .success:
+//                    if let value = response.result.value {
+//                        let json = JSON(value)
+//                        for recipe in json["drinks"].arrayValue {
+//                            //                                    self.cocktail?.update(recipe)
+//                        }
+//                    }
+//                    //                            self.tableView.reloadData()
+//                    
+//                case .failure(let error):
+//                    print(error)
+//                }
+//            }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
 }
